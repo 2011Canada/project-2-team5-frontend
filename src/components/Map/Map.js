6ba2d581e45c9161233bd757a02519c9f6f69e7c
TristanSBrennan <tristan.s.brennan@gmail.com>
2021-01-11 16:11:55 -0500

    map and drawer integration part 1

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,6 +2,9 @@ import React from 'react';
 import MapImage from './northatlanticmap.jpg';
 import './Map.css';
 import Button from '@material-ui/core/Button';
+import LocationDrawer from '../LocationDrawer';
+
+import paris_photo from '../../location_photos/paris.jpg'
 
 function WorldMap() {
   return <img src={MapImage} alt="Logo" resizeMode="cover" />;
@@ -18,6 +21,7 @@ function ParentMap() {
 /*END OF FUNCTION THAT FIRES WHEN THE BUTTONS ARE PRESSED*/
 
   return (
+    //all-containing component
     <div class="mapcontainer">
         <div class="mapfunction">
             {/* <WorldMap /> */}
@@ -46,7 +50,49 @@ function ParentMap() {
         </Button>
       </div>
     </div>
+    //end of all-containing component
   );
 }
 
-export default ParentMap;
+/*
+WHAT FOLLOWS IS THE TEST DATA FOR THE DUMMY VERSION OF THE LOCATION DRAWER
+*/
+
+let testLocation = {
+  id: 0,
+  locationName: 'Paris',
+  description: 'This is a test description of Paris',
+  adjacent: [
+    { id: 0, name: 'London' },
+    { id: 1, name: 'Berlin' },
+  ],
+  image: paris_photo,
+};
+
+//TODO
+let setLocation = () => {};
+
+/*
+WHAT FOLLOWS IS THE EXPORTED FUNCTION, WHICH TESTS THE DUMMY VERSION OF THE LOCATION DRAWER
+*/
+
+function SuperParent(){
+  if(true){
+    return(
+      <div>
+        <ParentMap />
+        <LocationDrawer location={testLocation} setLocation={setLocation} />
+      </div>
+    );
+  }
+  else{
+    return(
+      <div>
+        <ParentMap />
+      </div>
+    );
+  }
+  
+}
+
+export default SuperParent;
