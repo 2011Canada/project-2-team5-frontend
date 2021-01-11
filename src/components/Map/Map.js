89f7e4610c8f942a9f68db01b9ded67f0f975d94
vincewangto <heng.w214@gmail.com>
2021-01-11 13:19:13 -0500

    Update Map.js

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,33 +4,41 @@ import './Map.css';
 import Button from '@material-ui/core/Button';
 
 function WorldMap() {
-    return (
-    <img src={MapImage} alt="Logo" />
-    );
+  return <img src={MapImage} alt="Logo" />;
 }
 
 function ParentMap() {
-    return (
-        <div class="mapcontainer">
-            <WorldMap/>
-            <div class="londonbutton">
-                <Button variant="contained" color="primary">
-                    London
-                </Button>
-            </div>
-            <div class="parisbutton">
-                <Button variant="contained" color="primary">
-                    Paris
-                </Button>
-            </div>
-            <div class="nycbutton">
-                <Button variant="contained" color="primary">
-                    New York
-                </Button>
-            </div>
-        </div>
-    
-    );
+  const handleButton = (location) => {
+    console.log(location);
+  };
+  return (
+    <div class="mapcontainer">
+      <WorldMap />
+      <div class="londonbutton">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleButton('London')}
+        >
+          London
+        </Button>
+      </div>
+      <div class="parisbutton">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleButton('Paris')}
+        >
+          Paris
+        </Button>
+      </div>
+      <div class="nycbutton">
+        <Button variant="contained" color="primary">
+          New York
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ParentMap;
